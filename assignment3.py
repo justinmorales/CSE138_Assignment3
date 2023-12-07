283a4f4bccc650d14ebef53d7356c167e26b6276
Justin Morales <magagod2@gmail.com>
2023-12-06 17:39:33 -0800

    so much debugging

--- assignment3.py
+++ assignment3.py
@@ -12,9 +12,7 @@ kv_store = {} # in-memory key-value store using dictionary
 sa_store = {} # in-memory store for storing the set of replicas among which the store is replicated
 vector_clock = [0,0,0]
 
-recovery_store = {"10.10.0.2:8090" : {},
-                  "10.10.0.3:8090" : {},
-                  "10.10.0.4:8090" : {}}
+
 
 # Create a function to increment vector clock based on each replica to ensure the causal consistency
 def inc_vector_clock():
@@ -28,20 +26,20 @@ def inc_vector_clock():
 def compare_vector_clock(v):
     if SOCKET_ADDRESS == "10.10.0.2:8090":
         if vector_clock[0] < v[1] or vector_clock[0] < v[2]:
-            return jsonify({"error": "Causal dependencies not satisfied; try again later"}), 503
+            return 503
     elif SOCKET_ADDRESS == "10.10.0.3:8090":
         if vector_clock[1] < v[0] or vector_clock[1] < v[2]:
-            return jsonify({"error": "Causal dependencies not satisfied; try again later"}), 503
+            return 503
     elif SOCKET_ADDRESS == "10.10.0.4:8090":
         if vector_clock[2] < v[0] or vector_clock[2] < v[1]:
-            return jsonify({"error": "Causal dependencies not satisfied; try again later"}), 503
+            return 503
 
 # Create a function to update vector clock based on each replica to ensure eventual consistency
 def update_vector_clock(v):
         vector_clock[0] = max(vector_clock[0], v[0])
         vector_clock[1] = max(vector_clock[1], v[1])
         vector_clock[2] = max(vector_clock[2], v[2])
-
+       
 # Create a function to check if the length of the key <key> is more than 50 characters
 def is_key_valid(key):
     return len(key) < 50
@@ -94,7 +92,6 @@ def handle_view():
         data = request.get_json()
         replica = data['socket-address']
         if replica in sa_store:
-            recovery_store[replica] = kv_store
             del sa_store[replica]
             if "broadcasted" in data:
                 return jsonify({"result": "deleted"}), 200
@@ -136,10 +133,18 @@ def handle_key(key):
             return jsonify({"error": "PUT request does not specify a value"}), 400
 
         causal_metadata = data.get('causal-metadata')
-        inc_vector_clock()
+        if "broadcasted" in data:
+            inc_vector_clock()
+        else:
+            if causal_metadata:
+                if compare_vector_clock(causal_metadata) == 503:
+                    return jsonify({"error": "Causal dependencies not satisfied; try again later"}), 503
+                update_vector_clock(causal_metadata)
+            inc_vector_clock()
         
         if causal_metadata:
-            compare_vector_clock(causal_metadata)
+            if compare_vector_clock(causal_metadata) == 503:
+                return jsonify({"error": "Causal dependencies not satisfied; try again later"}), 503
             update_vector_clock(causal_metadata)
         result = "created" if key not in kv_store else "replaced"
         kv_store[key] = value
@@ -155,7 +160,6 @@ def handle_key(key):
                 try:
                     #inc_vector_clock()
                     url = f"http://{replica}/kvs/{key}"
-                    method = request.method
                     r = requests.put(url, json={"value": value, "causal-metadata": vector_clock, "broadcasted": "true"}, timeout=0.5)
                     # update vector clock after request is returned
                     if r and (r.status_code == 200 or r.status_code == 201):
@@ -163,7 +167,7 @@ def handle_key(key):
                 
                 except requests.exceptions.ConnectionError:
                     url = f"http://{SOCKET_ADDRESS}/view"
-                    requests.delete(url, json={"socket-address": replica})
+                    requests.delete(url, json={"socket-address": replica, "on-operation": "PUT", "key": key, "value": value})
                     # try again
             
         return jsonify({"result": result, "causal-metadata": vector_clock}), 200 if result == "replaced" else 201
@@ -190,7 +194,9 @@ def handle_key(key):
         causal_metadata = data.get('causal-metadata')
 
         if causal_metadata:
-            compare_vector_clock(causal_metadata)
+            if compare_vector_clock(causal_metadata) == 503:
+                return jsonify({"error": "Causal dependencies not satisfied; try again later"}), 503
+            
             #compare = [x > y for x, y in zip(causal_metadata, vector_clock)]
             #if any(compare):
             #    return jsonify({"error": "Causal dependencies not satisfied; try again later"}), 503
@@ -218,9 +224,17 @@ def handle_key(key):
         # – 503 (Service Unavailable) {"error": "Causal dependencies not satisfied; try again later"}
         data = request.get_json()
         causal_metadata = data.get('causal-metadata')
-        inc_vector_clock()
+        if "broadcasted" in data:
+            inc_vector_clock()
+        else:
+            if causal_metadata:
+                if compare_vector_clock(causal_metadata) == 503:
+                    return jsonify({"error": "Causal dependencies not satisfied; try again later"}), 503
+                update_vector_clock(causal_metadata)
+            inc_vector_clock()
         if causal_metadata:
-            compare_vector_clock(causal_metadata)
+            if compare_vector_clock(causal_metadata) == 503:
+                return jsonify({"error": "Causal dependencies not satisfied; try again later"}), 503
             update_vector_clock(causal_metadata)
         # If the key <key> exists in the store, then remove it.
         # – Response code is 200 (Ok).
@@ -257,14 +271,17 @@ def get_key_list():
     # This method returns a list of all keys and values in the store.
     # – Response code is 200 (Ok).
     # – Response body is JSON {"key1": "value1", "key2": "value2", ...}.
-    data = request.get_json('socket-address')
-    socket_address = str(data)  # Convert data to a string
-    
-    recovery_kvs = {}
-    recovery_kvs = copy.deepcopy(socket_address)
-    #recovery_kvs = recovery_store[socket_address]
-    return jsonify({"recovery_data": recovery_kvs, "causal-metadata": vector_clock}), 200
-    
+    data = request.get_json()
+    replica = data['socket-address']  # Remove unnecessary conversion to string
+
+    # Synchronize the vector clock
+    if replica == "10.10.0.2:8090":
+        vector_clock[0] = max(vector_clock[1], vector_clock[2])
+    elif replica == "10.10.0.3:8090":
+        vector_clock[1] = max(vector_clock[0], vector_clock[2])
+    elif replica == "10.10.0.4:8090":
+        vector_clock[2] = max(vector_clock[0], vector_clock[1])
+    return jsonify({"recovery_data": kv_store, "causal-metadata": vector_clock}), 200
     
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=8090)
